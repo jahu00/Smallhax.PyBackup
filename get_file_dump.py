c05450a1ce8f4c81beb6749a8b9890efbfa0f92e
jahu00 <jahu.00@gmail.com>
2025-06-16 19:01:23 +0200

    Outputting some information from CLI scripts

--- get_file_dump.py
+++ get_file_dump.py
@@ -7,5 +7,9 @@ parser.add_argument("--output", type=str, help="output JSON")
 
 args = parser.parse_args()
 
+print(f"Scanning {args.path}")
 dump = FileDump.from_path(args.path)
+count = len(dump.files)
+print(f"Found {count} files and directories")
+print(f"Saving output to {args.output}")
 dump.save_to_file(args.output)
--- get_file_operations.py
+++ get_file_operations.py
@@ -1,14 +1,17 @@
 import argparse
+from itertools import groupby
 from classes import FileDump
 
 parser = argparse.ArgumentParser(description="Compares file dumps and prepares an operation list for syncing destination to source")
 parser.add_argument("--src", type=str, help="source path/JSON")
 parser.add_argument("--dst", type=str, help="destination path/JSON")
-parser.add_argument("--move", type=bool, help="allow move operation", default=False)
+parser.add_argument("--move", type=bool, help="allow move operation", default=True)
 parser.add_argument("--min", type=int, help="min file size for move operation (in bytes)", default=1048576)
 parser.add_argument("--output", type=str, help="output JSON")
 
 args = parser.parse_args()
+
+print(f"Comparing {args.src} to {args.dst}")
 
 if args.src.endswith(".json"):
     src_dump = FileDump.from_file(args.src)
@@ -21,4 +24,8 @@ else:
     dst_dump = FileDump.from_path(args.dst)
 
 operations = src_dump.compare(dst_dump, args.move, args.min)
+for key, group in groupby(operations.operations, lambda x: x.operation):
+   print(f"{len(list(group))} {key} operations")
+
+print(f"Saving output to {args.output}")
 operations.save_to_file(args.output)
